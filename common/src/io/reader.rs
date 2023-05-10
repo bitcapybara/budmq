7862b18ebf506c8efecec41042b070c693192cbb
bitcapybara <yuxing.bitcapybara@gmail.com>
2023-05-10 19:13:26 +0800

    add reader response

--- common/src/io/reader.rs
+++ common/src/io/reader.rs
@@ -1,18 +1,26 @@
-use futures::StreamExt;
+use futures::{SinkExt, StreamExt};
 use log::error;
 use s2n_quic::{connection::StreamAcceptor, stream::BidirectionalStream};
-use tokio::{select, sync::mpsc};
+use tokio::{
+    select,
+    sync::{mpsc, oneshot},
+};
 use tokio_util::{codec::Framed, sync::CancellationToken};
 
 use crate::protocol::{Packet, PacketCodec};
 
+pub struct PacketRequest {
+    packet: Packet,
+    res_tx: oneshot::Sender<Option<Packet>>,
+}
+
 pub struct Reader {
     acceptor: StreamAcceptor,
-    tx: mpsc::Sender<Packet>,
+    tx: mpsc::Sender<PacketRequest>,
 }
 
 impl Reader {
-    pub fn new(acceptor: StreamAcceptor) -> (Self, mpsc::Receiver<Packet>) {
+    pub fn new(acceptor: StreamAcceptor) -> (Self, mpsc::Receiver<PacketRequest>) {
         let (tx, rx) = mpsc::channel(1);
         (Self { acceptor, tx }, rx)
     }
@@ -44,7 +52,7 @@ impl Reader {
 
     async fn listen_on_stream(
         mut framed: Framed<BidirectionalStream, PacketCodec>,
-        tx: mpsc::Sender<Packet>,
+        tx: mpsc::Sender<PacketRequest>,
         token: CancellationToken,
     ) {
         loop {
@@ -58,10 +66,22 @@ impl Reader {
                         }
                         None => return
                     };
-                    if let Err(e) = tx.send(packet).await {
+                    let (res_tx, res_rx) = oneshot::channel();
+                    if let Err(e) = tx.send(PacketRequest { packet, res_tx }).await {
                         error!("io::reader send packet error: {e}");
                     }
-                    // TODO send response to framed
+                    // TODO sync wait
+                    match res_rx.await {
+                        Ok(Some(packet)) => {
+                            if let Err(e) = framed.send(packet).await {
+                                error!("io::reader send response error: {e}")
+                            }
+                        }
+                        Err(_) => {
+                            error!("io::reader res_tx dropped without send");
+                        }
+                        Ok(None) => {/* no need to send response */}
+                    }
                 }
                 _ = token.cancelled() => {
                     return
