34990b19d4c5bfc26977fa556925ed783936b4de
bitcapybara <yuxing.bitcapybara@gmail.com>
2023-05-10 23:35:53 +0800

    io::reader: async wait for response and send

--- common/src/io/reader.rs
+++ common/src/io/reader.rs
@@ -1,11 +1,16 @@
+use std::sync::Arc;
+
 use futures::{SinkExt, StreamExt};
 use log::error;
 use s2n_quic::{connection::StreamAcceptor, stream::BidirectionalStream};
 use tokio::{
     select,
-    sync::{mpsc, oneshot},
+    sync::{mpsc, oneshot, Mutex},
 };
-use tokio_util::{codec::Framed, sync::CancellationToken};
+use tokio_util::{
+    codec::{FramedRead, FramedWrite},
+    sync::CancellationToken,
+};
 
 use crate::protocol::{Packet, PacketCodec};
 
@@ -29,8 +34,8 @@ impl Reader {
         loop {
             select! {
                 res = self.acceptor.accept_bidirectional_stream() => {
-                    let framed = match res {
-                        Ok(Some(stream)) => Framed::new(stream, PacketCodec),
+                    let stream = match res {
+                        Ok(Some(stream)) => stream,
                         Ok(None) => {
                             token.cancel();
                             return
@@ -41,7 +46,7 @@ impl Reader {
                             return
                         }
                     };
-                    tokio::spawn(Self::listen_on_stream(framed, self.tx.clone(), token.child_token()));
+                    tokio::spawn(listen_on_stream(stream, self.tx.clone(), token.child_token()));
                 }
                 _ = token.cancelled() => {
                     return
@@ -49,30 +54,36 @@ impl Reader {
             }
         }
     }
+}
 
-    async fn listen_on_stream(
-        mut framed: Framed<BidirectionalStream, PacketCodec>,
-        tx: mpsc::Sender<PacketRequest>,
-        token: CancellationToken,
-    ) {
-        loop {
-            select! {
-                res = framed.next() => {
-                    let packet = match res {
-                        Some(Ok(packet)) => packet,
-                        Some(Err(e)) => {
-                            error!("io::reader decode frame error: {e}");
-                            continue;
-                        }
-                        None => return
-                    };
-                    let (res_tx, res_rx) = oneshot::channel();
-                    if let Err(e) = tx.send(PacketRequest { packet, res_tx }).await {
-                        error!("io::reader send packet error: {e}");
+async fn listen_on_stream(
+    stream: BidirectionalStream,
+    tx: mpsc::Sender<PacketRequest>,
+    token: CancellationToken,
+) {
+    let (recv, send) = stream.split();
+    let (mut recv_framed, send_framed) = (
+        FramedRead::new(recv, PacketCodec),
+        Arc::new(Mutex::new(FramedWrite::new(send, PacketCodec))),
+    );
+    loop {
+        select! {
+            res = recv_framed.next() => {
+                let packet = match res {
+                    Some(Ok(packet)) => packet,
+                    Some(Err(e)) => {
+                        error!("io::reader decode frame error: {e}");
+                        continue;
                     }
-                    // TODO sync wait
+                    None => return
+                };
+                let (res_tx, res_rx) = oneshot::channel();
+                // async wait for response
+                let send_framed = send_framed.clone();
+                tokio::spawn(async move {
                     match res_rx.await {
                         Ok(Some(packet)) => {
+                            let mut framed = send_framed.lock().await;
                             if let Err(e) = framed.send(packet).await {
                                 error!("io::reader send response error: {e}")
                             }
@@ -82,10 +93,13 @@ impl Reader {
                         }
                         Ok(None) => {/* no need to send response */}
                     }
+                });
+                if let Err(e) = tx.send(PacketRequest { packet, res_tx }).await {
+                    error!("io::reader send packet error: {e}");
                 }
-                _ = token.cancelled() => {
-                    return
-                }
+            }
+            _ = token.cancelled() => {
+                return
             }
         }
     }
