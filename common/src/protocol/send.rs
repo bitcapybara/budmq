1e603e5b6a822ad9033c093b1ddb42eaa2b007cc
bitcapybara <yuxing.bitcapybara@gmail.com>
2023-05-11 16:50:06 +0800

    use codec derive on Send packet

--- common/src/protocol/send.rs
+++ common/src/protocol/send.rs
@@ -1,38 +1,7 @@
-use bytes::{BufMut, Bytes};
-
-use super::{get_u64, read_bytes, write_bytes, Codec, Header, PacketType, Result};
-
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, codec_derive::Codec)]
 pub struct Send {
     pub request_id: u64,
     pub message_id: u64,
     pub consumer_id: u64,
     pub payload: Bytes,
-}
-
-impl Codec for Send {
-    fn decode(mut buf: bytes::Bytes) -> Result<Self> {
-        let request_id = get_u64(&mut buf)?;
-        let message_id = get_u64(&mut buf)?;
-        let consumer_id = get_u64(&mut buf)?;
-        let payload = read_bytes(&mut buf)?;
-        Ok(Self {
-            request_id,
-            message_id,
-            consumer_id,
-            payload,
-        })
-    }
-
-    fn encode(&self, buf: &mut bytes::BytesMut) -> Result<()> {
-        buf.put_u64(self.request_id);
-        buf.put_u64(self.message_id);
-        buf.put_u64(self.consumer_id);
-        write_bytes(buf, &self.payload);
-        Ok(())
-    }
-
-    fn header(&self) -> Header {
-        Header::new(PacketType::Send, 8 + 8 + 8 + self.payload.len() + 2)
-    }
 }
--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,6 +1,102 @@
+use core::panic;
+
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput, Field, Fields, Type};
 
 #[proc_macro_derive(Codec)]
-pub fn codec(_input: TokenStream) -> TokenStream {
-    todo!()
+pub fn codec(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let struct_ident = input.ident;
+
+    let Data::Struct(struct_data) = input.data else {
+        panic!("must derive on a struct")
+    };
+    let Fields::Named(fields) = struct_data.fields else {
+        panic!("must derive on named fields")
+    };
+    let field_idents = fields.named.iter().map(|field| {
+        let field_ident = &field.ident;
+        quote! {
+            #field_ident
+        }
+    });
+    let field_read_methods = fields.named.iter().map(|field| {
+        let field_ident = &field.ident;
+        match get_field_type(field).as_str() {
+            "u64" => quote! {
+                let #field_ident = super::get_u64(&mut buf)?;
+            },
+            "String" => quote! {
+                let #field_ident = super::read_string(&mut buf)?;
+            },
+            "Bytes" => quote! {
+                let #field_ident = super::read_bytes(&mut buf)?;
+            },
+            _ => panic!("unsupported field types"),
+        }
+    });
+    let field_put_methods = fields.named.iter().map(|field| {
+        let field_ident = &field.ident;
+        match get_field_type(field).as_str() {
+            "u64" => quote! {
+                buf.put_u64(self.#field_ident);
+            },
+            "String" => quote! {
+                super::write_string(buf, &self.#field_ident);
+            },
+            "Bytes" => quote! {
+                super::write_bytes(buf, &self.#field_ident);
+            },
+            _ => panic!("unsupported field types"),
+        }
+    });
+    let field_sizes = fields.named.iter().map(|field| {
+        let field_ident = &field.ident;
+        match get_field_type(field).as_str() {
+            "u64" => quote! {
+                8
+            },
+            "String" | "Bytes" => quote! {
+                2 + self.#field_ident.len()
+            },
+            "u16" => quote! {
+                2
+            },
+            _ => panic!("unsupported field types"),
+        }
+    });
+    quote! {
+        use bytes::{BufMut, Bytes};
+        impl super::Codec for #struct_ident {
+            fn decode(mut buf: bytes::Bytes) -> super::Result<Self> {
+                #(#field_read_methods)*
+                Ok(Self {
+                    #(#field_idents),*
+                })
+            }
+
+            fn encode(&self, buf: &mut bytes::BytesMut) -> super::Result<()> {
+                #(#field_put_methods)*
+                Ok(())
+            }
+
+            fn header(&self) -> super::Header {
+                super::Header::new(super::PacketType::#struct_ident, #(#field_sizes) + *)
+            }
+        }
+    }
+    .into()
+}
+
+fn get_field_type(field: &Field) -> String {
+    let Type::Path(path) = &field.ty else {
+        panic!("must derive on path field type")
+    };
+    path.path
+        .segments
+        .last()
+        .expect("field type ident not found")
+        .ident
+        .to_string()
 }
