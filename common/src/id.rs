31bb8f448e1a4321cc18fa890b226188bdcf53b9
bitcapybara <yuxing.bitcapybara@gmail.com>
2023-05-15 17:28:50 +0800

    add serialId

--- common/src/id.rs
+++ common/src/id.rs
@@ -1,5 +1,29 @@
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    Arc,
+};
+
 use chrono::Local;
 
 pub fn next_id() -> u64 {
     Local::now().timestamp_millis() as u64
+}
+
+#[derive(Clone)]
+pub struct SerialId(Arc<AtomicUsize>);
+
+impl Default for SerialId {
+    fn default() -> Self {
+        SerialId(Arc::new(AtomicUsize::new(0)))
+    }
+}
+
+impl SerialId {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn get(&self) -> u64 {
+        self.0.fetch_add(1, Ordering::Relaxed) as u64
+    }
 }
