3a9a58a548e04ae3379f3844f221694d4df1f8bc
bitcapybara <yuxing.bitcapybara@gmail.com>
2023-05-17 11:55:08 +0800

    client connection writer

--- client/src/connection.rs
+++ client/src/connection.rs
@@ -1,12 +1,97 @@
-mod error;
-mod sender;
+mod reader;
+mod writer;
 
-use std::{net::SocketAddr, sync::Arc};
+use std::{
+    net::SocketAddr,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+};
 
-use tokio::sync::{oneshot, Mutex};
+use bud_common::{id::SerialId, protocol::Packet};
+use tokio::sync::{mpsc, oneshot, Mutex};
 use tokio_util::sync::CancellationToken;
 
-use self::error::SharedError;
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    CommonIo(bud_common::io::Error),
+}
+
+impl std::error::Error for Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl From<bud_common::io::Error> for Error {
+    fn from(_e: bud_common::io::Error) -> Self {
+        todo!()
+    }
+}
+
+impl<T> From<mpsc::error::SendError<T>> for Error {
+    fn from(_e: mpsc::error::SendError<T>) -> Self {
+        todo!()
+    }
+}
+
+impl From<oneshot::error::RecvError> for Error {
+    fn from(_e: oneshot::error::RecvError) -> Self {
+        todo!()
+    }
+}
+
+#[derive(Clone)]
+pub struct SharedError {
+    error_set: Arc<AtomicBool>,
+    error: Arc<Mutex<Option<Error>>>,
+}
+
+impl SharedError {
+    pub fn new() -> SharedError {
+        SharedError {
+            error_set: Arc::new(AtomicBool::new(false)),
+            error: Arc::new(Mutex::new(None)),
+        }
+    }
+
+    /// used by stream reader, check if error need to reconnect
+    pub fn is_set(&self) -> bool {
+        self.error_set.load(Ordering::Relaxed)
+    }
+
+    /// used by stream reader, check if error need to reconnect
+    pub async fn remove(&self) -> Option<Error> {
+        let mut lock = self.error.lock().await;
+        let error = lock.take();
+        self.error_set.store(false, Ordering::Release);
+        error
+    }
+
+    /// used by stream writer, set when error occurs in writing packets
+    pub async fn set(&self, error: Error) {
+        let mut lock = self.error.lock().await;
+        *lock = Some(error);
+        self.error_set.store(true, Ordering::Release);
+    }
+}
+
+pub enum Event {
+    /// register consumer to client reader
+    AddConsumer {
+        consumer_id: u64,
+        /// sender held by client reader
+        /// receiver held by client consumer
+        sender: mpsc::UnboundedSender<Packet>,
+    },
+    /// unregister consumer from client reader
+    DelConsumer { consumer_id: u64 },
+}
 
 pub enum ConnectionState {
     Connected(Arc<Connection>),
@@ -16,7 +101,8 @@ pub enum ConnectionState {
 /// held by producer/consumer
 pub struct Connection {
     /// use sender to send all packets
-    sender: sender::Sender,
+    request_id: SerialId,
+    event_tx: mpsc::UnboundedSender<Event>,
     /// error to indicate weather producer/consumer need to get a new connection
     error: SharedError,
     /// self-managed token
---
+++ client/src/connection/writer.rs
@@ -0,0 +1,193 @@
+use std::{sync::Arc, time::Duration};
+
+use bud_common::{
+    id::next_id,
+    io::{stream::Request, writer},
+    protocol::{Packet, Ping, Response, ReturnCode},
+};
+use log::{error, trace, warn};
+use s2n_quic::connection::Handle;
+use tokio::{
+    select,
+    sync::{mpsc, oneshot, Mutex},
+    time::timeout,
+};
+use tokio_util::sync::CancellationToken;
+
+use crate::WAIT_REPLY_TIMEOUT;
+
+use super::{Result, SharedError};
+
+pub struct OutgoingMessage {
+    pub packet: Packet,
+    pub res_tx: oneshot::Sender<Option<ReturnCode>>,
+}
+
+pub struct Writer {
+    /// sender message to io::writer
+    sender: mpsc::Sender<Request>,
+    /// connection error
+    error: SharedError,
+    token: CancellationToken,
+}
+
+impl Writer {
+    pub async fn new(
+        handle: Handle,
+        ordered: bool,
+        error: SharedError,
+        token: CancellationToken,
+    ) -> Result<Self> {
+        let (sender, receiver) = mpsc::channel(1);
+        let writer = writer::Writer::builder(receiver, handle, token.clone())
+            .ordered(ordered)
+            .build()
+            .await?;
+        tokio::spawn(writer.run());
+        Ok(Self {
+            sender,
+            error,
+            token,
+        })
+    }
+    pub async fn run(
+        mut self,
+        server_rx: Arc<Mutex<mpsc::UnboundedReceiver<OutgoingMessage>>>,
+        keepalive: u16,
+    ) {
+        let keepalive = Duration::from_millis(keepalive as u64);
+        let mut server_rx = server_rx.lock().await;
+        let mut ping_err_count = 0;
+        loop {
+            select! {
+                res = timeout(keepalive, server_rx.recv()) => {
+                    match res {
+                        Ok(res) => {
+                            let Some(msg) = res else {
+                                trace!("connector::writer: receive none, exit");
+                                return;
+                            };
+                            if let Err(e) = self.write(msg).await {
+                                self.error.set(e).await;
+                                return
+                            }
+                        }
+                        Err(_) => {
+                            match self.ping().await {
+                                Ok(Some(_)) => {},
+                                Ok(None) => {
+                                    trace!("connector::writer::run: miss pong packet");
+                                    ping_err_count += 1;
+                                    if ping_err_count >= 3 {
+                                        return
+                                    }
+                                },
+                                Err(_) => {
+                                    return
+                                },
+                            }
+                        }
+                    }
+                }
+                _ = self.token.cancelled() => {
+                    return
+                }
+            }
+        }
+    }
+
+    async fn ping(&mut self) -> Result<Option<()>> {
+        trace!("connector::writer::run: send PING packet to server, open bi stream");
+        // send ping
+        let (res_tx, res_rx) = oneshot::channel();
+        let request = Request {
+            packet: Packet::Ping(Ping {
+                request_id: next_id(),
+            }),
+            res_tx,
+        };
+        self.sender.send(request).await?;
+        trace!("connector::writer::run: waiting for PONG packet");
+        match timeout(WAIT_REPLY_TIMEOUT, res_rx).await {
+            Ok(Ok(Ok(Packet::Pong(_)))) => Ok(Some(())),
+            Ok(Ok(Ok(_))) => {
+                error!("received unexpected packet, expected PONG packet");
+                Ok(None)
+            }
+            Ok(Ok(Err(e))) => {
+                error!("send ping error: {e}");
+                Ok(None)
+            }
+            Ok(Err(e)) => {
+                error!("client ping send endpoint dropped");
+                Err(e.into())
+            }
+            Err(_) => {
+                error!("waiting for PONG packet timeout");
+                Ok(None)
+            }
+        }
+    }
+
+    async fn write(&self, msg: OutgoingMessage) -> Result<()> {
+        let (packet, client_res_tx) = (msg.packet, msg.res_tx);
+        let packet_type = packet.packet_type();
+        // send publish message
+        trace!("connector::writer::run: send {packet_type} message to server");
+        let (res_tx, res_rx) = oneshot::channel();
+        let request = Request { packet, res_tx };
+        if let Err(e) = self.sender.send(request).await {
+            if client_res_tx.send(None).is_err() {
+                error!("client writer wait channel dropped by user");
+            }
+            return Err(e.into());
+        }
+        // wait for ack
+        trace!("connector::writer::run: waiting for server response");
+        let error = self.error.clone();
+        tokio::spawn(async move {
+            match Self::wait(res_rx).await {
+                Ok(Some(resp)) => {
+                    if client_res_tx.send(Some(resp.code)).is_err() {
+                        warn!("client writer wait channel dropped");
+                    }
+                }
+                Ok(None) => {}
+                Err(e) => {
+                    error.set(e).await;
+                }
+            }
+        });
+
+        Ok(())
+    }
+
+    async fn wait(
+        res_rx: oneshot::Receiver<bud_common::io::Result<Packet>>,
+    ) -> Result<Option<Response>> {
+        let resp = match timeout(WAIT_REPLY_TIMEOUT, res_rx).await {
+            Ok(Ok(Ok(Packet::Response(resp)))) => resp,
+            Ok(Ok(Ok(_))) => {
+                error!("client writer: expected Packet::Response");
+                return Ok(None);
+            }
+            Ok(Ok(Err(e))) => {
+                error!("client writer decode frame error: {e}");
+                return Err(e.into());
+            }
+            Ok(Err(e)) => {
+                error!("client writer: res_tx dropped");
+                return Err(e.into());
+            }
+            Err(_) => {
+                error!("client writer wait for reply timeout");
+                return Ok(None);
+            }
+        };
+        trace!(
+            "connector::writer::run: receive reponse from server: {}",
+            resp.code
+        );
+        Ok(Some(resp))
+    }
+}
