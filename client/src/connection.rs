1bb3f56c3b29caee3c497edbd45cee5f3d4cb53d
bitcapybara <yuxing.bitcapybara@gmail.com>
2023-05-17 15:04:32 +0800

    client connection::get_connection

--- client/src/connection.rs
+++ client/src/connection.rs
@@ -2,14 +2,18 @@ mod reader;
 mod writer;
 
 use std::{
+    io,
     net::SocketAddr,
+    ops::DerefMut,
     sync::{
         atomic::{AtomicBool, Ordering},
         Arc,
     },
 };
 
-use bud_common::id::SerialId;
+use bud_common::{id::SerialId, mtls::MtlsProvider};
+use log::trace;
+use s2n_quic::client;
 use tokio::sync::{mpsc, oneshot, Mutex};
 use tokio_util::sync::CancellationToken;
 
@@ -19,8 +23,8 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
-    CommonIo(bud_common::io::Error),
     Disconnect,
+    Canceled,
 }
 
 impl std::error::Error for Error {}
@@ -45,6 +49,24 @@ impl<T> From<mpsc::error::SendError<T>> for Error {
 
 impl From<oneshot::error::RecvError> for Error {
     fn from(_e: oneshot::error::RecvError) -> Self {
+        todo!()
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(_e: io::Error) -> Self {
+        todo!()
+    }
+}
+
+impl From<s2n_quic::provider::StartError> for Error {
+    fn from(_e: s2n_quic::provider::StartError) -> Self {
+        todo!()
+    }
+}
+
+impl From<s2n_quic::connection::Error> for Error {
+    fn from(_e: s2n_quic::connection::Error) -> Self {
         todo!()
     }
 }
@@ -98,7 +120,7 @@ pub enum Event {
 
 pub enum ConnectionState {
     Connected(Arc<Connection>),
-    Connecting(Option<oneshot::Receiver<Arc<Connection>>>),
+    Connecting(Vec<oneshot::Sender<Result<Arc<Connection>>>>),
 }
 
 /// held by producer/consumer
@@ -112,22 +134,104 @@ pub struct Connection {
     token: CancellationToken,
 }
 
+impl Connection {
+    fn new() -> Self {
+        // create reader from s2n_quic::Acceptor
+        // create writer from s2n_quic::Handle
+        todo!()
+    }
+
+    fn clone_one(&self, _ordered: bool) -> Self {
+        // create writer from s2n_quic::Handle
+        todo!()
+    }
+
+    fn is_valid(&self) -> bool {
+        !self.error.is_set()
+    }
+}
+
 /// connection manager held client
 pub struct ConnectionHandle {
     addr: SocketAddr,
+    server_name: String,
+    provider: MtlsProvider,
     conn: Arc<Mutex<ConnectionState>>,
 }
 
 impl ConnectionHandle {
     pub fn new(_addr: &SocketAddr) -> Self {
-        // create reader from s2n_quic::Acceptor
         todo!()
     }
 
     /// get the connection in manager, setup a new writer
-    pub async fn get_connection(&self, _ordered: bool) -> Arc<Connection> {
-        // create writer from s2n_quic::Handle
-        todo!()
+    pub async fn get_connection(&self, _ordered: bool) -> Result<Arc<Connection>> {
+        let rx = {
+            let mut conn = self.conn.lock().await;
+            match conn.deref_mut() {
+                ConnectionState::Connected(c) => {
+                    if c.is_valid() {
+                        return Ok(c.clone());
+                    } else {
+                        None
+                    }
+                }
+                ConnectionState::Connecting(waiters) => {
+                    let (tx, rx) = oneshot::channel();
+                    waiters.push(tx);
+                    Some(rx)
+                }
+            }
+        };
+
+        match rx {
+            Some(rx) => match rx.await {
+                Ok(conn) => conn,
+                Err(_) => Err(Error::Canceled),
+            },
+            None => self.connect().await,
+        }
+    }
+
+    async fn connect(&self) -> Result<Arc<Connection>> {
+        let new_conn = match self.connect_inner().await {
+            Ok(c) => c,
+            Err(e) => {
+                let mut conn = self.conn.lock().await;
+                if let ConnectionState::Connecting(waiters) = conn.deref_mut() {
+                    for tx in waiters.drain(..) {
+                        tx.send(Err(Error::Canceled)).ok();
+                    }
+                }
+                return Err(e);
+            }
+        };
+        let mut conn = self.conn.lock().await;
+        match conn.deref_mut() {
+            ConnectionState::Connected(c) => *c = new_conn.clone(),
+            ConnectionState::Connecting(waiters) => {
+                for tx in waiters.drain(..) {
+                    tx.send(Ok(new_conn.clone())).ok();
+                }
+            }
+        }
+        Ok(new_conn)
+    }
+
+    async fn connect_inner(&self) -> Result<Arc<Connection>> {
+        // TODO loop retry backoff
+        // unwrap safe: with_tls error is infallible
+        trace!("connector::connect: create client and connect");
+        let client: client::Client = client::Client::builder()
+            .with_tls(self.provider.clone())
+            .unwrap()
+            .with_io("0.0.0.0:0")?
+            .start()?;
+        let connector =
+            s2n_quic::client::Connect::new(self.addr).with_server_name(self.server_name.as_str());
+        let mut connection = client.connect(connector).await?;
+        connection.keep_alive(true)?;
+        Ok(Arc::new(Connection::new()))
     }
 }
 
